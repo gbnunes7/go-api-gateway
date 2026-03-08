00f8e6cca7a918f9ec8185fa4ba5c358b37e491b
Gabriel Melo <bielrj170@gmail.com>
2026-03-08 18:03:26 -0300

    chore: initialize go project

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
+
+}
