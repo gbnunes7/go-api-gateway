91ed5a28f3de67104551a0bddd635bd8d9965d4a
Gabriel Melo <bielrj170@gmail.com>
2026-03-09 23:37:20 -0300

    refactor: change to use concorrency goroutines

--- internal/usecase/get_dashboard_usecase.go
+++ internal/usecase/get_dashboard_usecase.go
@@ -6,6 +6,21 @@ import (
 	"context"
 )
 
+type usersResult struct {
+	users []dto.User
+	err   error
+}
+
+type ordersResult struct {
+	orders []dto.Order
+	err    error
+}
+
+type billingsResult struct {
+	billings []dto.Billing
+	err      error
+}
+
 type GetDashboardUsecase struct {
 	usersProvider    contract.UsersProvider
 	ordersProvider   contract.OrdersProvider
@@ -25,28 +40,48 @@ func NewGetDashboardUsecase(
 }
 
 func (u *GetDashboardUsecase) Execute(ctx context.Context) (dto.DashboardResponse, error) {
-	users, err := u.usersProvider.GetUsers(ctx)
-	if err != nil {
-		return dto.DashboardResponse{}, err
+	usersChan := make(chan usersResult, 1)
+	ordersChan := make(chan ordersResult, 1)
+	billingsChan := make(chan billingsResult, 1)
+
+	go func() {
+		users, err := u.usersProvider.GetUsers(ctx)
+		usersChan <- usersResult{users: users, err: err}
+	}()
+
+	go func() {
+		orders, err := u.ordersProvider.GetOrders(ctx)
+		ordersChan <- ordersResult{orders: orders, err: err}
+	}()
+
+	go func() {
+		billings, err := u.billingsProvider.GetBillings(ctx)
+		billingsChan <- billingsResult{billings: billings, err: err}
+	}()
+
+	ru := <-usersChan
+	ro := <-ordersChan
+	rb := <-billingsChan
+
+	if ru.err != nil {
+		return dto.DashboardResponse{}, ru.err
 	}
-	orders, err := u.ordersProvider.GetOrders(ctx)
-	if err != nil {
-		return dto.DashboardResponse{}, err
+	if ro.err != nil {
+		return dto.DashboardResponse{}, ro.err
 	}
-	billings, err := u.billingsProvider.GetBillings(ctx)
-	if err != nil {
-		return dto.DashboardResponse{}, err
+	if rb.err != nil {
+		return dto.DashboardResponse{}, rb.err
 	}
 
 	billingByOrderID := make(map[string]dto.Billing)
-	for _, b := range billings {
+	for _, b := range rb.billings {
 		billingByOrderID[b.OrderID] = b
 	}
 
 	var out []dto.UserWithOrders
-	for _, u := range users {
+	for _, u := range ru.users {
 		userRow := dto.UserWithOrders{ID: u.ID, Name: u.Name, Email: u.Email}
-		for _, o := range orders {
+		for _, o := range ro.orders {
 			if o.UserID != u.ID {
 				continue
 			}
