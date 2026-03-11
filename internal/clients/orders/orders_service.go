39c6b94c7ade9fa7448840c33a8c4daa04a9cba7
Gabriel Melo <bielrj170@gmail.com>
2026-03-11 00:33:17 -0300

    feat: update internal/clients/orders/orders_service.go

--- internal/clients/orders/orders_service.go
+++ internal/clients/orders/orders_service.go
@@ -1,13 +1,19 @@
 package orders
 
 import (
-	"api-gateway-go/internal/constants"
-	"api-gateway-go/internal/dto"
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+
+	"api-gateway-go/internal/constants"
+	"api-gateway-go/internal/dto"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/propagation"
 )
 
 type Client struct {
@@ -24,32 +30,39 @@ func NewClient(baseURL string, httpClient *http.Client) *Client {
 
 func (c *Client) GetOrders(ctx context.Context) ([]dto.Order, error) {
 	u, err := url.JoinPath(c.baseURL, "/orders")
-
 	if err != nil {
 		return nil, fmt.Errorf("orders client: build url error: %w", err)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
+	tracer := otel.Tracer("api-gateway")
+	ctx, span := tracer.Start(ctx, "http.client.orders")
+	defer span.End()
+	span.SetAttributes(attribute.String("http.url", u))
 
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
 	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.RecordError(err)
 		return nil, fmt.Errorf("orders client: create request error: %w", err)
 	}
 
-	traceID, _ := ctx.Value(constants.TraceIDKey).(string)
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
 
+	traceID, _ := ctx.Value(constants.TraceIDKey).(string)
 	if traceID != "" {
 		req.Header.Set(string(constants.TraceIDKey), traceID)
 	}
 
 	resp, err := c.httpClient.Do(req)
-
 	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+		span.RecordError(err)
 		return nil, fmt.Errorf("orders client: do request error: %w", err)
 	}
-
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		span.SetStatus(codes.Error, fmt.Sprintf("status %d", resp.StatusCode))
 		return nil, fmt.Errorf("orders client: status code error: %d", resp.StatusCode)
 	}
 
